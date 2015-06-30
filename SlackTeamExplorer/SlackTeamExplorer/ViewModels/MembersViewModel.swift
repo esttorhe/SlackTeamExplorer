5c82c9f81b35747a47af18fbd6fa3461b97b6f26
esttorhe <me@estebantorr.es>
2015-06-29 19:20:22 -0600

    [WIP] Added basic CoreData support
    - App now checks if there's connection to internet
      + If not reaches from DB
      + if there is retrieves the data
    
    # TODO:
    - Avoid duplicate entries; right now there's no check if the model was already inserted or not, and thus, subsequent runs with connection will keep growing the DB exponentially

--- SlackTeamExplorer/SlackTeamExplorer/ViewModels/MembersViewModel.swift
+++ SlackTeamExplorer/SlackTeamExplorer/ViewModels/MembersViewModel.swift
@@ -6,15 +6,20 @@
 //  Copyright (c) 2015 Esteban Torres. All rights reserved.
 //
 
+// Native Frameworks
+import CoreData
+
 // Pods
 import ReactiveViewModel
 import ReactiveCocoa
+import ReachabilitySwift
 
 public class MembersViewModel: RVMViewModel {
     private var members = [Member]()
     let beginLoadingSignal: RACSignal = RACSubject()
     let endLoadingSignal: RACSignal = RACSubject()
     let updateContentSignal: RACSignal = RACSubject()
+    let reachability = Reachability.reachabilityForInternetConnection()
     
     var numberOfSections: Int {
         get { return 1 }
@@ -31,45 +36,97 @@ public class MembersViewModel: RVMViewModel {
     override init() {
         super.init()
         
+        reachability.startNotifier()
         self.didBecomeActiveSignal.subscribeNext { [unowned self] active in
             self.active = false
             // Notify caller that network request is about to begin
             if let beginSignal = self.beginLoadingSignal as? RACSubject { beginSignal.sendNext(nil) }
-            SlackProvider.request(.TeamUsersList) { (data, status, response, error) -> () in
-                // Notify caller that network request ended
-                if let endSignal = self.endLoadingSignal as? RACSubject { endSignal.sendNext(nil) }
-                
-                // Check if there was a network error, and if so notify back and cancel processing
-                // data.
-                if let err = error where err.code != 0 {
-                    if let updateSignal = self.updateContentSignal as? RACSubject {
-                        updateSignal.sendError(err)
-                    }
-                    
-                    return
+        
+            if self.reachability.isReachable() {
+                self.loadDataFromWeb()
+            } else {
+                self.loadDataFromDB()
+            }
+        }
+    }
+    
+    // MARK: - Internal Helpers
+    
+    internal func loadDataFromWeb() {
+        SlackProvider.request(.TeamUsersList) { (data, status, response, error) -> () in
+            // Notify caller that network request ended
+            if let endSignal = self.endLoadingSignal as? RACSubject { endSignal.sendNext(nil) }
+            
+            // Check if there was a network error, and if so notify back and cancel processing
+            // data.
+            if let err = error where err.code != 0 {
+                if let updateSignal = self.updateContentSignal as? RACSubject {
+                    updateSignal.sendError(err)
                 }
                 
-                // Parse data
-                if let data = data {
-                    var localError: NSError?
-                    if let json: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &localError) {
-                        if let members = json["members"] as? Array<Dictionary<NSObject, AnyObject>>,
-                            context = (UIApplication.sharedApplication().delegate as? AppDelegate)?.managedObjectContext {
-                                // Map the array of dictionaries into Member models removing possible `nil`s.
-                                self.members = members.map { Member.memberInContext(context, json: $0) }.filter{ $0 != nil }.map{ $0! }
-                                
-                                // Report back the new data.
-                                if let updateSignal = self.updateContentSignal as? RACSubject {
-                                    updateSignal.sendNext(self.members)
-                                }
-                        }
-                    } else {
-                        if let updateSignal = self.updateContentSignal as? RACSubject {
-                            updateSignal.sendError(localError)
-                        }
+                return
+            }
+            
+            // Parse data
+            if let data = data {
+                var localError: NSError?
+                if let json: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options: .MutableContainers, error: &localError) {
+                    if let members = json["members"] as? Array<Dictionary<NSObject, AnyObject>>,
+                        appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate,
+                        context = appDelegate.managedObjectContext {
+                            // Map the array of dictionaries into Member models removing possible `nil`s.
+                            self.members = members.map { Member.memberInContext(context, json: $0) }.filter{ $0 != nil }.map{ $0! }
+                            dispatch_async(dispatch_get_main_queue()) {
+                                appDelegate.saveContext()
+                            }
+                            
+                            // Report back the new data.
+                            if let updateSignal = self.updateContentSignal as? RACSubject {
+                                updateSignal.sendNext(self.members)
+                            }
+                    }
+                } else {
+                    if let updateSignal = self.updateContentSignal as? RACSubject {
+                        updateSignal.sendError(localError)
                     }
                 }
             }
+        }
+    }
+    
+    internal func loadDataFromDB() {
+        if let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate,
+            context = appDelegate.managedObjectContext {
+                let fetch = NSFetchRequest(entityName: "Member")
+                var innerError: NSError?
+                if let currentMembers = context.executeFetchRequest(fetch, error: &innerError) as? Array<Member> {
+                    // Notify caller that network request ended
+                    if let endSignal = self.endLoadingSignal as? RACSubject { endSignal.sendNext(nil) }
+                    
+                    // Check if there was a network error, and if so notify back and cancel processing
+                    // data.
+                    if let err = innerError where err.code != 0 {
+                        if let updateSignal = self.updateContentSignal as? RACSubject {
+                            updateSignal.sendError(err)
+                        }
+                        
+                        return
+                    }
+                    
+                    self.members = currentMembers
+                    
+                    // Report back the new data.
+                    if let updateSignal = self.updateContentSignal as? RACSubject {
+                        updateSignal.sendNext(self.members)
+                    }
+                } else {
+                    if let updateSignal = self.updateContentSignal as? RACSubject {
+                        let err = NSError(domain: "es.estebantorr.SlackTeamExplorer.MembersViewModel",
+                            code: -667,
+                            userInfo: [NSLocalizedDescriptionKey: "Unable to retrieve `Member` models from the database"])
+                        updateSignal.sendError(err)
+                    }
+                }
         }
     }
 }
